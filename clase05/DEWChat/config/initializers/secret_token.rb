d70ff3c1d0b4941933ac57e31cc5c91d6c68de34
Daso <daalsuol@gmail.com>
2013-05-28 15:50:02 -0500

    chat inicio

---
+++ clase05/DEWChat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWChat::Application.config.secret_token = 'bc0c062ef6dbee57f48c4cd75ddf5a89b1170b59748199b65c59956a4d0a3739f27261f280f09c47489b901badc6d02c40a401fceea0fad151c66912d095d97c'
