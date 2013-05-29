bfd3983b55366cacbef681d9e3dfc34d4819266e
upc <stevensljpm@gmail.com>
2013-05-28 20:26:34 -0500

    chat

--- clase05/DEWChat/app/models/message.rb
+++ clase05/DEWChat/app/models/message.rb
@@ -1,2 +1,14 @@
 class Message < ActiveRecord::Base
+  Pusher.app_id = 44793
+  Pusher.key = '8af1a78c2496c405d491'
+  Pusher.secret = '8a0c0b5b6f4d2c6af4e2'
+
+  after_create :send_to_pusher
+
+  def send_to_pusher
+  	logger.info "Hola!"
+  	Pusher['Chat2013'].trigger("message:create", self.to_json)
+  end
+  
+
 end
